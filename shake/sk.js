ee4a47045f8d0c0fd1ade0e7ca28b2c46a6bd20e
AnYingYing <1121256285@qq.com>
2017-12-11 16:14:00 +0800

    2
    2

---
+++ shake/sk.js
@@ -0,0 +1,29 @@
+
+$(document).ready(function(){
+
+	// var vibrateInterval;
+
+	// // 开始震动
+	// function startVibrate(duration) {
+	//     navigator.vibrate(duration);
+	// }
+
+	// // 停止震动
+	// function stopVibrate() {
+	//     // 清除间隔和停止持续振动
+	//     if(vibrateInterval) clearInterval(vibrateInterval);
+	//     navigator.vibrate(0);
+	// }
+
+	// //在给定的持续时间和间隔时开始持续的振动
+	// //假定一个数字值
+	// function startPeristentVibrate(duration, interval) {
+	//     vibrateInterval = setInterval(function() {
+	//         startVibrate(duration);
+	//     }, interval);
+	// }
+
+	// startVibrate(5000);
+
+	alert("888888");
+});
